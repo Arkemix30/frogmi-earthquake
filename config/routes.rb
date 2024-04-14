40af0230d85046ce7be2acbe1544529db3c9821f
Arkemix30 <silvacompte@gmail.com>
2024-04-14 15:29:00 -0600

    feat(routes): setting routes for the project

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,15 @@ Rails.application.routes.draw do
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
+  get 'up' => 'rails/health#show', as: :rails_health_check
 
   # Defines the root path route ("/")
   # root "posts#index"
+
+  namespace :api do
+    get '/features/count', to: 'features#count'
+    resources :features, only: %i[index show] do
+      resources :comments, only: %i[index show create]
+    end
+  end
 end
