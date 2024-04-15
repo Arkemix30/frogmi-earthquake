af0a07ae89651ddccf675057ed842ce8a6fe0c29
Arkemix30 <silvacompte@gmail.com>
2024-04-14 22:11:12 -0600

    refactor(application): add dotenv for reading env files
    also enabled extension in db schema for postgresqlk

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,12 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.1].define(version: 2024_04_14_192115) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "comments", force: :cascade do |t|
     t.string "body"
-    t.integer "feature_id", null: false
+    t.bigint "feature_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["feature_id"], name: "index_comments_on_feature_id"
